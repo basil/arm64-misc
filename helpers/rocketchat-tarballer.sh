4ab77c2de0ec888be991cc22768e8cf7b3dc6d4b
Lachlan Marie <lchlnm@pm.me>
2025-11-10 08:29:48 +0800

    Increased version in rocketchat tarballer and changed filenames

--- helpers/rocketchat-tarballer.sh
+++ helpers/rocketchat-tarballer.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-version=4.9.1
+version=4.9.2
 
-tar -xzf $version.tar.gz
+tar -xzf Rocket.Chat.Electron-$version.tar.gz
 cd Rocket.Chat.Electron-$version/
 yarn install --immutable --mode=skip-build
-tar -czf rocketchat-desktop-$version-yarn-cache.tar.xz .yarn/
-mv rocketchat-desktop-$version-yarn-cache.tar.xz ../rocketchat-desktop-$version-yarn-cache.tar.xz
+tar -czf Rocket.Chat.Electron-$version-yarn-cache.tar.xz .yarn/
+mv Rocket.Chat.Electron-$version-yarn-cache.tar.xz ../Rocket.Chat.Electron-$version-yarn-cache.tar.xz
